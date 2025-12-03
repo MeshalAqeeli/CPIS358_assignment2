65c0f23ff5c31bb0f805cf316dc282f10b1c53d2
MeshalAqeeli <meshal.aqily11@gmail.com>
2025-12-04 02:51:48 +0300

    added controller

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReceiptTracker.Models;
 
 namespace ReceiptTracker.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<ReceiptTracker.Models.Receipt> Receipt { get; set; } = default!;
     }
 }
