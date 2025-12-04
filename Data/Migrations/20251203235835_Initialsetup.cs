520b6ef29ea2a87d370f053c1c8b50e925055356
MeshalAqeeli <meshal.aqily11@gmail.com>
2025-12-04 03:04:51 +0300

    create migration

---
+++ Data/Migrations/20251203235835_Initialsetup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReceiptTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Receipt",
+                columns: table => new
+                {
+                    receiptId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    storeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Purchasedate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    warrantyEnd = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receipt", x => x.receiptId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Receipt");
+        }
+    }
+}
--- Models/Receipt.cs
+++ Models/Receipt.cs
@@ -5,7 +5,7 @@
         public int receiptId { get; set; }
         public string productName { get; set; }
         public string storeName { get; set; }
-        public DateTime Purchasedate { get; set; }
+        public DateTime purchaseDate { get; set; }
         public decimal price { get; set; }
         public string description { get; set; }
 
