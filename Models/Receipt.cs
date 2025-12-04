1dc7f1cf2f20aa69d33702f4e7f33436cb920b57
MeshalAqeeli <meshal.aqily11@gmail.com>
2025-12-04 04:06:05 +0300

    edited some naming mistakes

--- Models/Receipt.cs
+++ Models/Receipt.cs
@@ -3,11 +3,11 @@
     public class Receipt
     {
         public int receiptId { get; set; }
-        public string SroductName { get; set; }
-        public string StoreName { get; set; }
+        public string productName { get; set; }
+        public string storeName { get; set; }
         public DateTime purchaseDate { get; set; }
         public decimal price { get; set; }
-        public string Description { get; set; }
+        public string description { get; set; }
 
         public DateTime warrantyEnd { get; set; }
 
