b0012b134fc242c1ff062a4b35cbd5315a32d4e0
MeshalAqeeli <meshal.aqily11@gmail.com>
2025-12-04 04:24:30 +0300

    edited warranty type in migration

--- Data/Migrations/20251203235835_Initialsetup.cs
+++ Data/Migrations/20251203235835_Initialsetup.cs
@@ -22,7 +22,7 @@ namespace ReceiptTracker.Data.Migrations
                     Purchasedate = table.Column<DateTime>(type: "datetime2", nullable: false),
                     price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
                     description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    warrantyEnd = table.Column<DateTime>(type: "int", nullable: false)
+                    warrantyEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
                 {
